1fc969e7b260e38b679dfdd563ff176707d0cd25
Starky <david990917@qq.com>
2020-04-13 16:28:40 +0800

    Course Summary

---
+++ Courses/CPP-Programming-Course/Textbook/Chapter6/global_num.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include <algorithm>
+using namespace std;
+
+int num;
+void f(int i) {
+	if (i == 4) {
+		cout << "finish" << endl;
+		return;
+	}
+	for (num = 0; num < 3; num++) {
+		cout << num << endl;
+		i++;
+		if (num % 2)f(i);
+		i--;
+	}
+}
+
+int main() {
+	f(1);
+	return 1;
+}
---
+++ Courses/CPP-Programming-Course/Textbook/Chapter7/Reference_Same_Address.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	int i = 1;
+	int& j1 = i;
+	int& j2 = j1;
+
+	cout << "int i = " << i << ", address = " << &i << endl;
+	cout << "int& j1 = " << j1 << ", address = " << &j1 << endl;
+	cout << "int& j2 = " << j1 << ", address = " << &j2 << endl;
+
+	/*int i = 1, address = 005FF774
+	int& j1 = 1, address = 005FF774
+	int& j2 = 1, address = 005FF774*/
+}
