29eb5ea9e1a2b885158e404302a50250aa5eed3a
Starky <david990917@qq.com>
2020-04-13 10:16:38 +0800

    Create sizeof.cpp

---
+++ Courses/CPP-Programming-Course/Textbook/Chapter2/sizeof.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() {
+	cout << sizeof(int) << endl;// 4
+	cout << sizeof(double) << endl;// 8
+	cout << sizeof(short) << endl;// 2
+	cout << sizeof(long) << endl;// 4
+	cout << sizeof(long long) << endl;// 8
+	cout << sizeof(float) << endl;// 4
+
+	return 0;
+}
