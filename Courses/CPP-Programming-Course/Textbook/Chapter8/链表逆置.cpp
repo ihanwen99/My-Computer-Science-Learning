3cb271ce1418be92cfa720e0062cee73f21959a7
Starky <david990917@qq.com>
2020-04-26 17:16:45 +0800

    Create 链表逆置.cpp

---
+++ Courses/CPP-Programming-Course/Textbook/Chapter8/链表逆置.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+
+
+struct ListNode {
+	int val;
+	ListNode* next;
+};
+
+ListNode* reverseList(ListNode* head) {
+	ListNode* pre = NULL, * curr = head;
+	while (curr != NULL) {
+		ListNode* tmp = curr->next;
+		curr->next = pre;
+		pre = curr;
+		curr = tmp;
+	}
+	return pre;
+}
